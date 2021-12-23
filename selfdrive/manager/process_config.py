8fb16d337118556e2810b1c1a41d21851a397093
bensonkwek <34688817+bensonkwek@users.noreply.github.com>
2021-12-23 09:07:52 -0800

    comment out uploader

--- selfdrive/manager/process_config.py
+++ selfdrive/manager/process_config.py
@@ -36,7 +36,7 @@ procs = [
   PythonProcess("thermald", "selfdrive.thermald.thermald", persistent=True),
   PythonProcess("timezoned", "selfdrive.timezoned", enabled=TICI, persistent=True),
   PythonProcess("tombstoned", "selfdrive.tombstoned", enabled=not PC, persistent=True),
-  PythonProcess("uploader", "selfdrive.loggerd.uploader", persistent=True),
+  #PythonProcess("uploader", "selfdrive.loggerd.uploader", persistent=True),
 
   # EON only
   PythonProcess("rtshield", "selfdrive.rtshield", enabled=EON),
